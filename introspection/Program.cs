758da06560206110a0cb18e03fcfe2fb1cad2e40
victorsmits <victor.smits@student.vinci.be>
2018-11-08 20:03:16 +0100

    CodeSource methode

--- introspection/Program.cs
+++ introspection/Program.cs
@@ -8,13 +8,41 @@ namespace introspection
     {
         public static void Main(string[] args)
         {
-            CodeSource("Item");
+            CodeSource("introspection.Item");
         }
 
-        public static void CodeSource(Object classe)
+        public static void CodeSource(string classe)
         {
-            Type type = classe.GetType();
-            Console.WriteLine(type);
+            BindingFlags flag = BindingFlags.DeclaredOnly |
+                                BindingFlags.Public |
+                                BindingFlags.NonPublic |
+                                BindingFlags.Instance;
+
+            Type type = Type.GetType(classe);
+
+            Console.WriteLine("----MethodInfo----\n");
+            foreach (MethodInfo Minfo in type.GetMethods(flag))
+            {
+                Console.WriteLine(Minfo.Name);
+            }
+
+            Console.WriteLine("\n----FieldInfo----\n");
+            foreach (FieldInfo Finfo in type.GetFields(flag))
+            {
+                Console.WriteLine(Finfo);
+            }
+
+            Console.WriteLine("\n----PropertyInfo----\n");
+            foreach (PropertyInfo Pinfo in type.GetProperties(flag))
+            {
+                Console.WriteLine(Pinfo);
+            }
+
+            Console.WriteLine("\n----ConstructorInfo----\n");
+            foreach (ConstructorInfo Cinfo in type.GetConstructors())
+            {
+                Console.WriteLine(Cinfo);
+            }
         }
     }
 }
